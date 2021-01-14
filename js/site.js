166f7d904e0e8124e308ffdd1fd5a08ef4400851
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-01-14 17:42:11 -0600

    set dynamic dropdown

--- js/site.js
+++ js/site.js
@@ -7,4 +7,39 @@ const eventData = [{event: "ComicCon",city: "New York",state: "New York",attenda
 {event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 40000,date: "06/01/2017"},
 {event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 45000,date: "06/01/2018"},
 {event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 50000,date: "06/01/2019"}]
+
+const dropdownMenu = document.querySelector('#dropdown-menu');
+
+// get states 
+const getStates = () => {
+  const uniqueStates = [];
+  let state;
+  eventData.forEach(eventObj => {
+    state = eventObj.state;
+    if (!uniqueStates.includes(state)) {
+        uniqueStates.push(state);
+    }
+  })
+  console.log(uniqueStates);
+  return uniqueStates;
+};
+
+
+
+
+
+const populateDropdownMenu = () => {
+  const states = getStates();
+
+  states.forEach(state => {
+    const aTag = document.createElement("a")
+    aTag.classList.add("dropdown-item");
+    aTag.setAttribute('href', `${state}`)
+    aTag.innerText = state;
+    dropdownMenu.appendChild(aTag);
+  })
+}
+
+
+document.onload = populateDropdownMenu();
                         
