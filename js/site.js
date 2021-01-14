c5b1392b8874692631a638eda7b5dc8172c7e9a2
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-01-14 16:04:54 -0600

    first commit

---
+++ js/site.js
@@ -0,0 +1,10 @@
+const eventData = [{event: "ComicCon",city: "New York",state: "New York",attendance: 240000,date: "06/01/2017"},
+{event: "ComicCon",city: "New York",state: "New York",attendance: 250000,date: "06/01/2018"},
+{event: "ComicCon",city: "New York",state: "New York",attendance: 257000,date: "06/01/2019"},
+{event: "ComicCon",city: "San Diego",state: "California",attendance: 130000,date: "06/01/2017"},
+{event: "ComicCon",city: "San Diego",state: "California",attendance: 140000,date: "06/01/2018"},
+{event: "ComicCon",city: "San Diego",state: "California",attendance: 150000,date: "06/01/2019"},
+{event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 40000,date: "06/01/2017"},
+{event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 45000,date: "06/01/2018"},
+{event: "HeroesCon",city: "Charlotte",state: "North Carolina",attendance: 50000,date: "06/01/2019"}]
+                        
