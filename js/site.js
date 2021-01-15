c22a0f30151083c349df51c9f6dd6408813b76a6
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-01-15 14:53:49 -0600

    get and display stat table data

--- js/site.js
+++ js/site.js
@@ -27,9 +27,6 @@ const getStates = () => {
   return uniqueStates;
 };
 
-
-
-
 // Create dropdown menu
 const populateDropdownMenu = () => {
   const states = getStates();
@@ -37,12 +34,14 @@ const populateDropdownMenu = () => {
   states.forEach(state => {
     const aTag = document.createElement('a')
     aTag.classList.add("dropdown-item");
-    aTag.setAttribute('href', `${state}`)
+    aTag.setAttribute('href', '#')
+    aTag.setAttribute('id', `${state}`);
     aTag.innerText = state;
     dropdownMenu.appendChild(aTag);
   })
 }
 
+// Populate table with data
 const populateTable = (dataSrc, table) => {
 
   dataSrc.forEach(eventObj => {
@@ -56,7 +55,76 @@ const populateTable = (dataSrc, table) => {
   })
 };
 
+// Get stats
+const getStats = (event) => {
+  const state = event.target.id;
+
+  let totalAttendance = 0;
+  let totalEvents = 0;
+  let mostAttended = 0;
+  let leastAttended = 0;
+
+  // get total attendance and total events
+  eventData.forEach(eventObj => {
+    if (eventObj.state === state )
+    {
+      totalAttendance += eventObj.attendance;
+      totalEvents ++;
+    }
+
+    if (eventObj.attendance > mostAttended) {
+      mostAttended = eventObj.attendance;
+    }
+  })
+
+  return ({
+    "State": state,
+    "Total Attendance": totalAttendance,
+    "Average Attendance": totalAttendance / totalEvents,
+    "Most Attended": mostAttended,
+    "Least Attended": leastAttended
+  });
+};
+
+const statTablePrinter = (stats) => {
+  const tableStats = document.querySelector('#table-stats');
+  tableStats.innerHTML = '';
+
+  const tableHead = document.createElement('thead');
+  const tableHeadRow = document.createElement('tr');
+  const tableHeadData = document.createElement('th');
+
+  const tableStatsBody = document.createElement('tbody');
+  
+  // Create table data
+  for (key in stats) {
+    if (key === 'State') {
+      tableHeadData.setAttribute('colspan', 2);
+      tableHeadData.innerText = `Stats For ${stats.State} Events`;
+      tableHeadRow.appendChild(tableHeadData);
+      tableHead.appendChild(tableHeadRow);
+      tableStats.appendChild(tableHead);
+    }
+    const tableBodyRow = document.createElement('tr');
+    const tableData1 = document.createElement('td');
+    tableData1.innerText = key;
+    const tableData2 = document.createElement('td');
+    tableData2.innerText = stats[key];
+console.log(tableData2);
+    tableBodyRow.appendChild(tableData1);
+    tableBodyRow.appendChild(tableData2);
+
+    tableStatsBody.appendChild(tableBodyRow);
+  }
+  tableStats.appendChild(tableStatsBody);
+}
+
+
 
 document.onload = populateDropdownMenu();
 document.onload = populateTable(eventData, tableMainBody);
-                        
+
+dropdownMenu.addEventListener('click', (event) => {
+  statTablePrinter(getStats(event));
+  // console.log(event.target.id);
+})
